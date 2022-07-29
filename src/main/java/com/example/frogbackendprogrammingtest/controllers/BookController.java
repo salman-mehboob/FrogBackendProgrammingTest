15c336447e193f72c8b758629d233b4cbf939349
salmanmehboob <salmanmehboob60@gmail.com>
2022-07-29 13:00:54 +0100

    servies

--- src/main/java/com/example/frogbackendprogrammingtest/controllers/BookController.java
+++ src/main/java/com/example/frogbackendprogrammingtest/controllers/BookController.java
@@ -5,17 +5,16 @@ import com.example.frogbackendprogrammingtest.domain.Book;
 import com.example.frogbackendprogrammingtest.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-@RequestMapping("/main")
+@Controller
 public class BookController {
-    @Autowired
     BookRepository bookRepository;
-
     @GetMapping("/book")
     public List<Book> getAllBooks() {
         return bookRepository.findAll();
--- src/main/java/com/example/frogbackendprogrammingtest/service/BookService.java
+++ src/main/java/com/example/frogbackendprogrammingtest/service/BookService.java
@@ -1,9 +1,38 @@
 package com.example.frogbackendprogrammingtest.service;
 
+import com.example.frogbackendprogrammingtest.DTO.BookException;
+import com.example.frogbackendprogrammingtest.domain.Book;
+import com.example.frogbackendprogrammingtest.repository.BookRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BookService {
+
+    private final Logger log = LoggerFactory.getLogger(BookService.class);
+    BookRepository bookRepository;
+
+    public Book save(Book book) {
+        log.debug("Request to save new book : {}", book);
+        return bookRepository.save(book);
+    }
+
+    public List<Book> findAllBooks() {
+        log.debug("Request to get all books");
+        return bookRepository.findAll();
+    }
+
+    public void delete(Long id){
+        log.debug("Request to delete user : {}", id);
+        Book book = bookRepository.findById(id).orElseThrow(() -> new BookException("Book", "id", id));
+        bookRepository.deleteById(id);
+    }
+
+
 
 
 }
